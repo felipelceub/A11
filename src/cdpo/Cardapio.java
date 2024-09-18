43b67ab6ed6e57ecda36c190fa9ad975624daa6d
Felipe Lara da Rocha Trevisol <felipe.trevisol@dinfor.uniceub>
2024-09-18 09:07:03 -0300

    a

---
+++ src/cdpo/Cardapio.java
@@ -0,0 +1,15 @@
+package cdpo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio{
+	List<String> listaNomes = new ArrayList<>();
+	List<String> listaDescricao = new ArrayList<>();
+	List<Boolean> listaAtivo = new ArrayList<>();
+	List<Double> listaPreco = new ArrayList<>();
+	
+	
+	
+	
+}
